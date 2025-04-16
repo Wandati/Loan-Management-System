6cc06e29825238a9a0167eda0b3370808e945fdd
Wandati <petersimon801@gmail.com>
2025-04-16 11:06:59 +0300

    Omitted unnecessary SQL statement in Loan Repository

--- src/main/java/com/credable/lms/repository/LoanRepository.java
+++ src/main/java/com/credable/lms/repository/LoanRepository.java
@@ -2,15 +2,12 @@ package com.credable.lms.repository;
 
 import com.credable.lms.model.Loan;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 public interface LoanRepository extends JpaRepository<Loan, Long> {
     Optional<Loan> findTopByCustomerNumberOrderByCreatedAtDesc(String customerNumber);
     boolean existsByCustomerNumberAndStatus(String customerNumber, String status);
-    @Query("SELECT COUNT(l) > 0 FROM Loan l WHERE l.customerNumber = :customerNumber AND l.status IN :statuses")
-    boolean existsByCustomerNumberAndStatusIn(@Param("customerNumber") String customerNumber, @Param("statuses") String[] statuses);
+    
 }
 
